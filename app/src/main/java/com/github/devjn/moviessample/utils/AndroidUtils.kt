5bdd61e729f25f0b27e27a34756d974b91c35191
devjn <devjn@jn-arts.com>
2018-08-27 16:50:17 +0300

    Implemented error handling of missing image

--- app/src/main/java/com/github/devjn/moviessample/utils/AndroidUtils.kt
+++ app/src/main/java/com/github/devjn/moviessample/utils/AndroidUtils.kt
@@ -1,7 +1,14 @@
 package com.github.devjn.moviessample.utils
 
+import android.graphics.drawable.Drawable
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.github.devjn.moviessample.App
 import com.github.devjn.moviessample.R
 
@@ -15,6 +22,22 @@ import com.github.devjn.moviessample.R
 fun ImageView.loadImage(url: String) {
     GlideApp.with(this).load(url).placeholder(R.drawable.ic_image)
             .centerCrop().into(this)
+}
+
+@BindingAdapter("imageUrl", "textView")
+fun ImageView.loadImage(url: String, textView: TextView) {
+    GlideApp.with(this).load(url).placeholder(R.drawable.ic_image)
+            .centerCrop().listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    textView.visibility = View.VISIBLE
+                    return false
+                }
+
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    textView.visibility = View.GONE
+                    return false
+                }
+            }).into(this)
 }
 
 object AndroidUtils {
